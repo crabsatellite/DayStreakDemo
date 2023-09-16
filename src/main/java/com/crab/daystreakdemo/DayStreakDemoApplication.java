4e2d59b8513352c472a9100be08391aef07d602f
Alex Lee <islicy@outlook.com>
2023-09-15 20:32:23 -0400

    initial commit

---
+++ src/main/java/com/crab/daystreakdemo/DayStreakDemoApplication.java
@@ -0,0 +1,13 @@
+package com.crab.daystreakdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayStreakDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DayStreakDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crab/daystreakdemo/DayStreakDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crab.daystreakdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayStreakDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
